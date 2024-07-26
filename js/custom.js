97877133a9d37e765d33a0496255a200778694d9
ratul-724 <rhratul306@gmai.com>
2024-07-26 19:45:29 +0600

    change

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,14 @@
 $('document').ready(function(){
-    $('.container button').click(function(){
+    $('.container .add').click(function(){
         let lastCatNum = $('.category-box:last').attr('cat-num');
-        let NewCatNum = +lastCatNum + 1;
+        let NewCatNum = + lastCatNum + 1;
         let NewCat = '<div class="category-box" cat-num="1"> \
-                         <input type="text" placeholder="Add New Category" class="input-box"> \
+                         <input type="text" placeholder="Add New Category" required class="input-box"> \
+                         <img src="img/close.png" alt="close" class="img"> \
                     </div>'
         $('.container form').append(NewCat);
     });
+    $('.img').click(function(){
+        $('.container form').remove(NewCat);
+    })
 });
